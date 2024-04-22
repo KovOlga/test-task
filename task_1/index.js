ff3369f11711d5dc09c9c90af1239f8154347ad3
KovOlga <switch.to.olga@yandex.ru>
2024-04-22 20:07:30 +0300

    added raw first task solution

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,30 @@
-import {encoded, translations} from './data.js'
+import { encoded, translations } from "./data.js";
 
-console.log("Let's rock")
-console.log(encoded, translations)
+console.log("Let's rock");
+// console.log(encoded, translations);
 
+const keysToDecode = Object.keys(encoded[0]).filter((key) => {
+  if (
+    encoded[0][key] &&
+    key.indexOf("Id") != -1 &&
+    key !== "groupId" &&
+    key !== "service" &&
+    key !== "formatSize" &&
+    key !== "ca"
+  ) {
+    return key;
+  }
+});
 
+// console.log("keysToDecode", keysToDecode);
 
-// console.log(decoded)
+const decoded = encoded.map((item) => {
+  keysToDecode.forEach((key) => {
+    if (translations[item[key]] !== undefined && item[key]) {
+      item[key] = translations[item[key]];
+    }
+  });
+  return item;
+});
+
+console.log(decoded);
